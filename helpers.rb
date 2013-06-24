b55e5e17c8bc084a21cdf88759e6a1a5bb416ad9
matt <matthewcopeland1@gmail.com>
2013-06-23 19:40:00 -0500

    added white-label .ais for icons, some user helpers, avatar images, tweaked utils to remove deprecated box-shadow mixin.

--- helpers.rb
+++ helpers.rb
@@ -2,6 +2,21 @@ def parameterize(data)
   data.gsub(/\s+/, "-").downcase
 end
 
+
 def nav_links
   []
+end
+
+
+def random_date
+  "#{rand(1970...2013)}/#{rand(1...12)}/#{rand(1...28)}"
+end
+
+
+def sampler(group, size)
+  smplr = []
+  size.times do
+    smplr.push(group.sample)
+  end
+  smplr.uniq
 end
---
+++ helpers/users_helper.rb
@@ -0,0 +1,47 @@
+def user_avatar_url
+  "/images/avatars/#{rand(1...5)}.png"
+end
+
+
+def admin_avatar_url
+  "/images/avatars/admin.png"
+end
+
+
+
+# random names from http://random-name-generator.info
+def user_names
+  [
+    "Miles Pereira",
+    "Brett Hyatt",
+    "Cassaundra Kaufman",
+    "Ping Fairbanks",
+    "Graham Lapointe",
+    "Lizeth Medlock",
+    "Anisa Howland",
+    "Lilli Fortenberry",
+    "Britt Isbell",
+    "Del Royer",
+    "Junior Petitt",
+    "Sixta Gamboa",
+    "Lashawn Malatesta",
+    "Pearl Mathisen",
+    "Elinore Mohamed",
+    "Charlena Granata",
+    "Tamika Seppala",
+    "Freddie Montez",
+    "Alejandro Katz",
+    "Hipolito Carrizales",
+    "Azzie Leverette",
+    "Marva Holley",
+    "Lanny Salzer",
+    "Rueben Bail",
+    "Eartha Buie",
+    "Joycelyn Rehn",
+    "Karlyn Strasburg",
+    "Malcolm Haddad",
+    "Kacie Carter",
+    "Monroe Carle",
+  ]
+end
+
